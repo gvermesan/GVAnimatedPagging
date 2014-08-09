0a31f822817996b2689e5ce6128e08cbef814433
Gabriel Vermesan <gabrielvermesan@MacBook-Air.local>
2014-08-09 14:21:52 +0300

    Initial Commit

---
+++ GVAnimatedPagging/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GVAnimatedPagging
+//
+//  Created by Gabriel Vermesan on 8/9/14.
+//  Copyright (c) 2014 Gabriel Vermesan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GVAppDelegate class]));
+    }
+}
---
+++ GVAnimatedPaggingTests/GVAnimatedPaggingTests.m
@@ -0,0 +1,34 @@
+//
+//  GVAnimatedPaggingTests.m
+//  GVAnimatedPaggingTests
+//
+//  Created by Gabriel Vermesan on 8/9/14.
+//  Copyright (c) 2014 Gabriel Vermesan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GVAnimatedPaggingTests : XCTestCase
+
+@end
+
+@implementation GVAnimatedPaggingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
